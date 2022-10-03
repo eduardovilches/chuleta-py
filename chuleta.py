206f4f0686e41ee5634bbbf037d0ee4cb9f5d739
Eduardo Vilzo <eduardo.angel124@gmail.com>
2022-10-03 11:45:47 -0500

    Update chuleta.py

--- chuleta.py
+++ chuleta.py
@@ -4,3 +4,17 @@
 #Example: data[i] list of range
 def indexExist(self, ls, i):
   return (0 <= i < len(ls)) or (-len(ls) <= i < 0)
+
+#sample input
+#5 4
+#1 2 3 4 5
+#Left Rotate
+def rotateLeft(d, arr):
+    i =1 
+    while i <= d:
+        current = arr[0]
+        arr.append(current)
+        del arr[0]
+        i += 1
+     
+    return arr
